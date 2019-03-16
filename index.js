17a8eb4d959adf1f9da40ce62de88a9a12972afb
culigan <culigan@gmail.com>
2019-03-16 15:41:54 -0600

    working on the database

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function getJCategories(request, response) {
         if (error || result == null || result.length != 1) {
             response.status(500).json({ success: false, data: error });
         } else {
-            var rows = json(result);
+            var rows = response.status(200).json(result);
             console.log(rows);
             response.render('jpage', rows);
         }
