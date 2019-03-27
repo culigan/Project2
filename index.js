a48a5e2c793c792a59261477f9cb0219f922348e
culigan <culigan@gmail.com>
2019-03-26 23:11:16 -0600

    working on more css

--- index.js
+++ index.js
@@ -93,7 +93,8 @@ app.get('/answer', function (request, response) {
 });
 
 app.post('/addScore', urlendcodedParser, function (request, response) {
-    if (typeof request.session.username === 'undefined')
+    if (typeof request.session.username === 'undefined') {
+        console.log("undefined");
         request.session.score = parseInt(request.body.score);
     else
         request.session.score = parseInt(request.body.score) + parseInt(request.session.score);
