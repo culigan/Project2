b43d4166af4c43ed58ab9f93c58f8da75978f3e7
culigan <culigan@gmail.com>
2019-03-26 20:45:51 -0600

    working on more css

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.get('/question', function (request, response) {
                 var data = JSON.parse(result.body);
                 jserv.category(category, function (errorT, responseT, resultTitle) {
                     if (!errorT && responseT.statusCode == 200) {
+                        console.log(responseT, resultTitle);
                         response.render('question', ({ category: resultTitle.title, difficulty: difficulty, answer: data[0].answer, question: data[0].question, type: "Jeopardy"}));
                     }
                 });
