c451a1df7195e21a696c12e517a05dc3a736732e
culigan <culigan@gmail.com>
2019-03-27 19:53:05 -0600

    working on more css

--- public/project2.js
+++ public/project2.js
@@ -46,7 +46,7 @@ function getJAnswer() {
 }
 
 function getCAnswer() {
-    var playerAnswer = document.getElementById('textans').value;
+    var playerAnswer = document.getElementsByName('textans').value;
     var correctAnswer = document.getElementById('correct').value;
     console.log("correct answer:" +playerAnswer + correctAnswer);
     var score = 0;
