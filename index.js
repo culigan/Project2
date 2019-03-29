5f083307e2753421f5847dab4b8029a36662ca9b
culigan <culigan@gmail.com>
2019-03-29 11:36:52 -0600

    working on more css

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/selection', function (request, response) {
     }
     
 });
-app.get('/question', urlendcodedParser, function (request, response) {
+app.get('/question', function (request, response) {
 
     if (request.query.command == "Play Jeopardy") {
         var category = request.query.cat;
@@ -51,11 +51,11 @@ app.get('/question', urlendcodedParser, function (request, response) {
         
         jserv.clues(options, function (error, result) {
             if (!error && response.statusCode == 200) {
-                var questions = "{ data: " + result.body + "}";
-                console.log("data : " + question);
+                var data = JSON.parse(result.body);
+                console.log("data : " + data);
                 jserv.category(category, function (errorT, responseT, resultTitle) {
                     if (!errorT && responseT.statusCode == 200) {
-                        response.render('question', ({ category: resultTitle.title, difficulty: questions.data[0].value, answer: questions.data[0].answer, question: questions.data[0].question, type: "Jeopardy", all: questions.data}));
+                        response.render('question', ({ category: resultTitle.title, difficulty: difficulty, answer: data[0].answer, question: data[0].question, type: "Jeopardy", all: data}));
                     }
                 });
             }
