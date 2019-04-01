fc541f602cd956bd1c74a4bb65adbf25f7d19b50
culigan <culigan@gmail.com>
2019-04-01 16:15:01 -0600

    refining

--- public/project2.js
+++ public/project2.js
@@ -54,14 +54,27 @@ function getClassic(cat, diff) {
             alert('ERROR!');
         }
     });
-}*/
+}*/t
+
+function replaceStringValues(stringValue, replaceValue) {
+    while (stringValue.search(stringValue) >= 0)
+        stringValue.replace(replaceValue, " ");
+}
 
 function getJAnswer() {
     var checked = true;
     document.getElementById('jeopardy').disabled = true;
     try {
         var apiAnswer = document.getElementById('hanswer').value.toUpperCase();
+        replaceStringValues(apiAnswer, " A ");
+        replaceStringValues(apiAnswer, " THE ");
+        replaceStringValues(apiAnswer, " OF ");
+        
         var playerAnswer = document.getElementById('textans').value.toUpperCase();
+        replaceStringValues(playerAnswer, " A ");
+        replaceStringValues(playerAnswer, " THE ");
+        replaceStringValues(playerAnswer, " OF ");
+
         var pointValue = document.getElementById('diffvalue').value;
         var score = 0;
 
@@ -120,7 +133,7 @@ function getCAnswer() {
         },
         Error: function () {
             alert('ERROR!');
-        }
+        }A
     });
 }
 
